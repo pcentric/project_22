0da2c31c21276a3f3659e542bcd243a1838fd994
Priyanshu S <priyanshu@connectcorp.co.in>
2021-01-30 13:01:17 +0530

    first commit

---
+++ src/containers/Tree.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { NodeContainer } from './';
+import { connect } from 'react-redux';
+import './Tree.css';
+
+class Tree extends Component {
+    render() {
+        const root = this.props.root;
+        return (
+            <div className="tree">
+                <NodeContainer key={root.id} {...root} />
+            </div>
+        );
+    }
+}
+
+Tree = connect(state => {
+    return {
+        root: Object.values(state).filter(n => n.type === 'Category')[0]
+    }
+})(Tree);
+
+
+export default Tree;
